c55a481b363c58dd3ff1528336e1151f8d8a4571
vanvo299 <luongvanvo29@gmail.com>
2026-01-05 23:21:30 +0700

    feat: implement feature with label in card and board

--- src/pages/board/ui/BoardPage.tsx
+++ src/pages/board/ui/BoardPage.tsx
@@ -1,13 +1,16 @@
 import { BoardDetailProvider } from "@/app/providers/BoardDetailProvider";
 import { ListProvider, CardDetailProvider } from "@/app/providers/index";
 import { BoardWidget } from "@/widgets/board/ui/BoardWidget";
+import { LabelProvider } from "@/app/providers/LabelProvider";
 
 export function BoardPage() {
     return (
         <BoardDetailProvider>
             <ListProvider>
                 <CardDetailProvider>
-                    <BoardWidget />
+                    <LabelProvider>
+                        <BoardWidget />
+                    </LabelProvider>
                 </CardDetailProvider>
             </ListProvider>
         </BoardDetailProvider>
